79f864a1ee84c520fc95d53b7a15002dc226e49c
Antoine Kingue <etorefr@gmail.com>
2023-03-16 11:46:49 +0100

    💡 add docs

--- improvements.py
+++ improvements.py
@@ -2,21 +2,44 @@ import random
 import requests
 
 class QuoteGenerator:
+    """
+    Classe qui permet de générer des phrases aléatoires.
+    """
     def __init__(self):
+        """
+        Constructeur de la classe QuoteGenerator.
+        
+        Initialise la liste des phrases vide.
+        """
         self.quotes = []
         
-    def add(self, quote):
+    def add(self, quote: str):
+        """
+        Méthode permettant d'ajouter une nouvelle phrase à la liste des phrases.
+        
+        @param quote (str): La phrase à ajouter à la liste des phrases.
+        """
         self.quotes.append(quote)
         
     def random(self):
+        """
+        Méthode permettant de générer une phrase aléatoire.
+        
+        @return (str): Une phrase aléatoire choisie parmi la liste des phrases.
+        """
         return random.choice(self.quotes)
 
+# Récupération des données depuis l'API
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
+
+# Génération d'un objet QuoteGenerator
 generator = QuoteGenerator()
 
+# Ajout des phrases de l'API à la liste des phrases
 for quote in response.json():
     generator.add(quote["title"])
 
+# Génération d'une phrase aléatoire et affichage dans la console à chaque appui sur une touche
 while True:
     input("Press any key to get a random quote:")
     print(generator.random())
--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 import random
 
 class QuoteGenerator:
+    """
+    Classe qui permet de générer des phrases aléatoires.
+    """
     def __init__(self):
+        """
+        Constructeur de la classe QuoteGenerator.
+        
+        Initialise la liste des phrases vide.
+        """
         self.quotes = []
         
-    def add(self, quote):
+    def add(self, quote: str):
+        """
+        Méthode permettant d'ajouter une nouvelle phrase à la liste des phrases.
+        
+        @param quote (str): La phrase à ajouter à la liste des phrases.
+        """
         self.quotes.append(quote)
         
     def random(self):
+        """
+        Méthode permettant de générer une phrase aléatoire.
+        
+        @return (str): Une phrase aléatoire choisie parmi la liste des phrases.
+        """
         return random.choice(self.quotes)
 
-# Phrases provenant de JSON Placeholder
-
+# Génération d'un objet QuoteGenerator
 generator = QuoteGenerator()
+
+# Ajout de quelques phrases à la liste des phrases
 generator.add("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
 generator.add("qui est esse")
 generator.add("ea molestias quasi exercitationem repellat qui ipsa sit aut")
 generator.add("eum et est occaecati")
 
+# Génération d'une phrase aléatoire et affichage dans la console
 print(generator.random())
